b5fca12d4dcbd984be2a8e081a4ee485eefb46a7
Brian Wilson <squarehippobills@gmail.com>
2021-06-09 12:31:23 -0400

    Initial Commit

---
+++ GetTSSongTitles/GetTSSongTitlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetTSSongTitlesApp.swift
+//  GetTSSongTitles
+//
+//  Created by Brian Wilson on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct GetTSSongTitlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
